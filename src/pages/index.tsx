de9b79e180240f320b6b6a43d6da011b058bfb47
Arthur <maskalenkasth@gmail.com>
2021-04-22 21:48:43 -0300

    eslint e prettier integrados

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,11 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
 
 type Props = {
-  title: string
-}
+  title: string;
+};
 
-const teste = 1;
-
-export default function Home( {title}: Props ) {
+export default function Home({ title }: Props) {
   return (
     <div className={styles.container}>
       <Head>
@@ -21,8 +19,7 @@ export default function Home( {title}: Props ) {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Get started by editing <code className={styles.code}>pages/index.js</code>
         </p>
 
         <div className={styles.grid}>
@@ -36,10 +33,7 @@ export default function Home( {title}: Props ) {
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
+          <a href="https://github.com/vercel/next.js/tree/master/examples" className={styles.card}>
             <h3>Examples &rarr;</h3>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
@@ -49,9 +43,7 @@ export default function Home( {title}: Props ) {
             className={styles.card}
           >
             <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
           </a>
         </div>
       </main>
@@ -62,10 +54,9 @@ export default function Home( {title}: Props ) {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          Powered by <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
-  )
+  );
 }
