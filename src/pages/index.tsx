c793479f32af33787d03cf8175d539100e1f99ff
Arthur <maskalenkasth@gmail.com>
2021-04-22 23:41:24 -0300

    testes implementados

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
 type Props = {
-  title: strsing;
+  title: string;
 };
 
 export default function Home({ title }: Props) {
